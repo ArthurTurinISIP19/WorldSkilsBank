3d793ed4563ea075e3b43536cee5f7a3bc0d40d9
Turin Arthur <ezeka1n@mail.ru>
2021-03-15 00:28:48 +0900

    Initial commit

---
+++ app/src/main/java/com/example/worldskilsbank/banks.java
@@ -0,0 +1,17 @@
+package com.example.worldskilsbank;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class banks extends AppCompatActivity implements View.OnClickListener {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.maps);
+    }
+    @Override
+    public void onClick(View v) {
+
+    }
+}
